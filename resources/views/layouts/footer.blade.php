456aa8e126ae7afb9a19383948ec2444663c291b
mdsazzad-hossain <mdsojib508@gmail.com>
2022-03-15 13:29:01 +0600

    intial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+</footer>
+</div>
+<script src="assets/plugins/jquery/jquery.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/dist/js/adminlte.min.js?v=3.2.0"></script>
+</body>
+
+</html>
